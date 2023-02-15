bf65edd52e83eed3a5169d32e2c4b50afca16398
Felipe Bressane <felipebressane.x@gmail.com>
2023-02-15 12:23:28 -0300

    First upload of CNNDeepLearning xRay

--- CNNPacket/__init__.py
+++
@@ -1,3 +0,0 @@
-from .model_CNN import modelosCNN
-from .param_CNN import paramCNN
-
--- UtilPacket/__init__.py
+++
@@ -1,7 +0,0 @@
-from .util_functions import fRsizeImages
-from .util_functions import fNumberFiles
-from .util_functions import fComonDivisors
-from .util_functions import fGetBestAccuracy
-from .util_functions import fPredictXRayLung
-from .util_functions import fReportHistoric
-
